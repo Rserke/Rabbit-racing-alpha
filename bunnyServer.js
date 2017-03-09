6d61aa55a78a0975d060aab86cb6fe35098771b6
nuoz <b-kifka@hotmail.com>
2017-03-09 15:45:45 +0100

    Meer server configuratie

--- bunnyServer.js
+++ bunnyServer.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
+const mongoClient = require("mongodb").MongoClient;
+const bodyParser = require("body-parser");
+
+app.get('/create', (req, res) => {
+    res.sendFile(__dirname + "/bunnyCreation.html")
+})
+
 
 app.get('/*', (req, res) => {
     res.sendFile(__dirname + "/index.html")
 }) 
-
 
 app.listen(1234)
